593bd39bcc0f428d181ec1eb1e2931512dca99bc
yamazaki2357 <yamazaki2357@gmail.com>
2023-05-17 22:56:55 +0900

    viii, ix

--- Week7-8/JavaScript-learning/hello.js
+++ Week7-8/JavaScript-learning/hello.js
@@ -1,9 +1,41 @@
 console.log('Hello World');
 
-const num_1 = 5;
-const num_2 = 3;
+// const num_1 = 5;
+// const num_2 = 3;
 
-let sum = num_1 + num_2;
-let difference = num_1 - num_2;
+// let sum = num_1 + num_2;
+// let difference = num_1 - num_2;
 
-console.log(sum + difference);
+// console.log(sum + difference);
+
+function greet(name) {
+  console.log(`Hello, ${name}!`);
+}
+
+greet("Hikaru");
+
+function checkTemperature(t) {
+  if (t >= 30) {
+    console.log("Hot");
+  } else if (t >= 15) {
+    console.log("Warm");
+  } else {
+    console.log("Cold");
+  }
+}
+
+checkTemperature(30)
+checkTemperature(15)
+checkTemperature(14)
+checkTemperature(18)
+
+function checkOddOrEven(n) {
+  if (n % 2 === 0) {
+    console.log("Even");
+  } else {
+    console.log("Odd");
+  }
+}
+
+checkOddOrEven(2)
+checkOddOrEven(1)
